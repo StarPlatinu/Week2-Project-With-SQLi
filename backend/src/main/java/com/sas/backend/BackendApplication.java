33b27f00bdb81281beb38f95b00921573440a898
StarPlatinu <thanhvp1509@gmail.com>
2023-08-16 02:16:52 +0700

    add service

--- backend/src/main/java/com/sas/backend/BackendApplication.java
+++ backend/src/main/java/com/sas/backend/BackendApplication.java
@@ -1,7 +1,10 @@
 package com.sas.backend;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class BackendApplication {
---
+++ backend/src/main/java/com/sas/backend/service/TodoService.java
@@ -0,0 +1,22 @@
+package com.sas.backend.service;
+
+import com.sas.backend.dto.TodoDto;
+
+import java.util.List;
+
+public interface TodoService {
+
+    TodoDto addTodo(TodoDto todoDto);
+
+    TodoDto getTodo(Long id);
+
+    List<TodoDto> getAllTodos();
+
+    TodoDto updateTodo(TodoDto todoDto, Long id);
+
+    void deleteTodo(Long id);
+
+    TodoDto completeTodo(Long id);
+
+    TodoDto inCompleteTodo(Long id);
+}
