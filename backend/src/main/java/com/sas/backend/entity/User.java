c44cec81e386e75fdf808f980eb9fa635d4b8db7
StarPlatinu <thanhvp1509@gmail.com>
2023-08-16 01:34:55 +0700

    update entity

--- backend/src/main/java/com/sas/backend/entity/User.java
+++ backend/src/main/java/com/sas/backend/entity/User.java
@@ -1,25 +1,36 @@
 package com.sas.backend.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
-@Table(name = "user_tbl")
+import java.util.Set;
+
+@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
-@Getter
-@Setter
-@Builder
+@Entity
+@Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    @Column(unique = true)
-    private String email;
-    private String password;
-    @Column(unique = true)
+    private Long id;
+    private String name;
+    @Column(nullable = false, unique = true)
     private String username;
-    private String bio;
-    private String image;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
